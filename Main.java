01575f38d72e5e5f304abdf8b31ae256a0de7c52
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-05 15:51:08 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+  
+ import java.util.ArrayList;
+
+  public class TestList {
+    public static void main(String[] args) {
+        ArrayList<String> pitches = new ArrayList<String>();
+        pitches.add("138");
+        pitches.add("129");
+        pitches.add("142");
+
+        // System.out.println(pitches.get(1));
+        // System.out.println(pitches.size());
+        // System.out.println(pitches.contains("142"));
+
+        // System.out.println(pitches.remove("129"));
+        // System.out.println(pitches.size());
+        System.out.println(pitches.remove(0));
+    }
+}
+    }
+}
