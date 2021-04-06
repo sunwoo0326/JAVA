63e56ae9a1d753d8c9b9826ad58ca14f2e7d733e
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-06 18:15:15 +0900

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+    String name;
+public void setName(String name) {
+    this.name = name;
+}
+
+    public static void main(String[] args) {
+        Animal cat =new Animal();
+        cat.setName("boby");
+
+        Animal dog = new Animal();
+        dog.setName("happy");
+
+        System.out.println(cat.name);
+        System.out.println(dog.name);
+    }
+}
