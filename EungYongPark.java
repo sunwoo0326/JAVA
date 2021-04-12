8d0a4b65db4f2328103da7f4e914471ec9741196
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-12 17:31:24 +0900

    Add files via upload

---
+++ EungYongPark.java
@@ -0,0 +1,9 @@
+package jump2java.house.person;
+
+
+public class EungYongPark extends HousePark {       
+    public static void main(String[] args) {
+        EungYongPark eyp = new EungYongPark();
+        System.out.println(eyp.lastname);       
+    }    
+}
---
+++ HousePark.java
@@ -0,0 +1,8 @@
+public class HousePark  {
+    static String lastname = "박";
+
+    public static void main(String[] args) {
+        HousePark pey = new HousePark();
+        HousePark pes = new HousePark();
+    }
+}
