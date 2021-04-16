72252395bcbaf13f96afb0b247815133c508a148
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-16 17:38:42 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,2 @@
-class Main {
-  public static void main(String[] args) {
-  
- import java.util.ArrayList;
-
-  public class TestList {
-    public static void main(String[] args) {
-        ArrayList<String> pitches = new ArrayList<String>();
-        pitches.add("138");
-        pitches.add("129");
-        pitches.add("142");
-
-        // System.out.println(pitches.get(1));
-        // System.out.println(pitches.size());
-        // System.out.println(pitches.contains("142"));
-
-        // System.out.println(pitches.remove("129"));
-        // System.out.println(pitches.size());
-        System.out.println(pitches.remove(0));
-    }
-}
-    }
-}
+Scanner sc = new Scanner(System.in);
+System.out.print("카운트다운 합니다.")
